d8b6482b2b4b6aeae21bc9af77739bc3daf63454
phichakkhana chanthubluang <phichakkhana_ch@cmu.ac.th>
2021-05-28 17:21:57 +0700

    commit xy

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 document.getElementById('ok').addEventListener('click', function (e) {
-    alert('ok ')
+    alert('ok is ok ')
     console.log(e)
 }
 )
 document.getElementById('cancel').addEventListener('click', function (e) {
-    alert('cancel ')
+    alert('ok is cancel ')
     console.log(e)
 }
 )
 document.getElementById('no').addEventListener('click', function (e) {
-    alert('no ')
+    alert('ok is no ')
     console.log(e)
 }
 )
+document.getElementById('cancel').addEventListener('mousemove', function (c) {
+    var x = c.client;
+    var y = c.client;
+    console.log('x:' + x + ',' + 'y:' + y)
+}
+)
+
+
+document.getElementById('ok').addEventListener('mousemove', function (c) {
+    var x = c.client;
+    var y = c.client;
+    console.log('x:' + x + ',' + 'y:' + y)
+}
+)
+
+document.getElementById('no').addEventListener('mousemove', function (c) {
+    var x = c.client;
+    var y = c.client;
+    console.log('x:' + x + ',' + 'y:' + y)
+}
+)
+
+
